6e134f7f29887a5390b5940a67db715f1dc1994c
GeniusHawlah <geniusdecode@gmail.com>
2024-04-08 20:50:09 +0100

    Fixed server component production error.

--- app/page.js
+++ app/page.js
@@ -2,12 +2,14 @@
 import Image from "next/image";
 import Left from "./components/landing-page/Left";
 import Right from "./components/landing-page/Right";
-import { generalStore } from "./a-store/zustand-store/generalStore";
+// import { generalStore } from "./a-store/zustand-store/generalStore";
 
 export default function Home() {
-const {setShowProfileDropdown} = generalStore()
+// const {setShowProfileDropdown} = generalStore()
   return (
-    <main  onClick={() => setShowProfileDropdown(false)} className="gen-padding lg:flex items-center  justify-between gap-x-5  w-full ">
+    <main
+      // onClick={() => setShowProfileDropdown(false)}
+      className="gen-padding lg:flex items-center  justify-between gap-x-5  w-full ">
     <Left/> <Right/>
     </main>
   );
